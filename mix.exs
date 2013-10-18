98154554e9da51d2a2e7e26250d6ee95bef9c5f1
Josh Adams <josh@isotope11.com>
2013-10-18 05:45:03 -0500

    Blank new mix project.  .git's awakening.

---
+++ mix.exs
@@ -0,0 +1,21 @@
+defmodule Zeldacat.Mixfile do
+  use Mix.Project
+
+  def project do
+    [ app: :zeldacat,
+      version: "0.0.1",
+      elixir: "~> 0.10.4-dev",
+      deps: deps ]
+  end
+
+  # Configuration for the OTP application
+  def application do
+    [mod: { Zeldacat, [] }]
+  end
+
+  # Returns the list of dependencies in the format:
+  # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
+  defp deps do
+    []
+  end
+end
